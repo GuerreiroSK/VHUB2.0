7473ecc568c1b395c9f07493b7a56d544b1d0d14
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-02-05 15:07:37 +0000

    deleted organzation entity file(mistake on create)

--- apps/server/src/repositories/users.repositorie.js
+++ apps/server/src/repositories/users.repositorie.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import User from '../entities/User.js'
 
 export function getUserTestData() {
     const user = new User(1, "joao", "joao@email.com", "mock-password");
 
     return user;
-=======
-export function getUserTestData() {
-    return { message: "User route working" };
->>>>>>> feature/organizations-layer
 }
--- apps/server/src/services/users.service.js
+++ apps/server/src/services/users.service.js
@@ -1,10 +1,6 @@
 import { getUserTestData } from "../repositories/users.repositorie.js";
 
 export function getUserTestMessage() {
-<<<<<<< HEAD
     const user = getUserTestData();
     return user.toPublic();
-=======
-    return getUserTestData();
->>>>>>> feature/organizations-layer
 }
