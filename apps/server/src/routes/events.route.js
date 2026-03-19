600a9bd39b0cf9ff40ab5788e3be89de1d3e59fc
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-03-19 17:25:26 +0000

    feat(events-routes): add GET /events/:id route

--- apps/server/src/routes/events.route.js
+++ apps/server/src/routes/events.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { eventsWithOrganizations, getEvents, eventTest } from '../controllers/events.controller.js'
+import { eventsWithOrganizations, getEvents, eventTest, getEventById } from '../controllers/events.controller.js'
 
 const testEventsRouter = express.Router();
 
-testEventsRouter.get('/', getEvents);
-
 testEventsRouter.get('/event_test', eventTest);
 
-testEventsRouter.get('/with-organizations', eventsWithOrganizations)
+testEventsRouter.get('/with-organizations', eventsWithOrganizations);
+
+testEventsRouter.get('/', getEvents);
+
+testEventsRouter.get('/:id', getEventById)
 
 export default testEventsRouter;
