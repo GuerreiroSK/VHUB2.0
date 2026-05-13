78966517dfcf209e6c72830bf565a53277fa86e4
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-05-13 20:31:39 +0100

    feat: add get user by id endpoint

--- apps/server/src/routes/users.route.js
+++ apps/server/src/routes/users.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { userTest, getAllUsers} from '../controllers/users.controller.js'
+import { userTest, getAllUsers, getUserById} from '../controllers/users.controller.js'
 
 const testUsersRouter = express.Router();
 
 testUsersRouter.get('/user_test', userTest);
 
 testUsersRouter.get('/', getAllUsers);
+
+testUsersRouter.get('/:id', getUserById);
 
 export default testUsersRouter;
