e95358cbcbe0a97d52ec137adc43a3f0fe8e9c72
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-05-14 18:06:45 +0100

    feat: add get organization by id endpoint

--- apps/server/src/routes/organizations.route.js
+++ apps/server/src/routes/organizations.route.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { organizationTest, getAllEventsByOrganizationId, getAllOrganizations } from '../controllers/organizations.controller.js'
+import { organizationTest, getAllEventsByOrganizationId, getAllOrganizations, getOrganizationById } from '../controllers/organizations.controller.js'
 
 const testOrganizationsRouter = express.Router();
 
 testOrganizationsRouter.get('/organization_test', organizationTest);
 
 testOrganizationsRouter.get('/', getAllOrganizations);
+
+testOrganizationsRouter.get('/:id', getOrganizationById);
 
 testOrganizationsRouter.get('/:id/events', getAllEventsByOrganizationId);
 
