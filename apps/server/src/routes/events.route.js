7d1f791719cf349e0c60d06812713ff0da47bbe2
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-06-18 15:36:03 +0100

    feat: add middleware authorization to delete events route

--- apps/server/src/routes/events.route.js
+++ apps/server/src/routes/events.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { eventsWithOrganizations, getEvents, getEventById, createEvent, updateEvent, deleteEvent } from '../controllers/events.controller.js';
 import { registerToAnEvent, getAttendeesByEventId, cancelRegistration } from '../controllers/event_attendees.controller.js';
+import { verifyToken } from '../middleware/auth.middleware.js';
 
 const eventsRouter = express.Router();
 
@@ -15,7 +16,7 @@ eventsRouter.post('/', createEvent );
 
 eventsRouter.patch('/:id', updateEvent);
 
-eventsRouter.delete('/:id', deleteEvent);
+eventsRouter.delete('/:id', verifyToken, deleteEvent);
 
 eventsRouter.post('/:id/attendees', registerToAnEvent);
 
