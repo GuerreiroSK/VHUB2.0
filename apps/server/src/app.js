e3c023115baf9690594736ed77fded4de6396085
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-06-21 23:44:16 +0100

    chore: add CORS to allow requests from frontend

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from 'cors'
 import usersRouter from "./routes/users.route.js"
 import organizationsRouter from "./routes/organizations.route.js"
 import eventsRouter from "./routes/events.route.js"
 import authRouter from "./routes/auth.route.js";
 
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.use('/api/users', usersRouter);
 app.use('/api/organizations', organizationsRouter);
