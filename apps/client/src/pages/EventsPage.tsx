52db72dea92963b20fd412dc71c88cb02d5bcc8c
JoaoPedroGuerreiro <joaoguerreirobusiness@gmail.com>
2026-06-22 19:29:46 +0100

    feat: fetch and display events on EventsPage

--- apps/client/src/pages/EventsPage.tsx
+++ apps/client/src/pages/EventsPage.tsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react';
+
+type Event = {
+    id: number
+    eventName: string
+    location: string
+    organizationId: number
+    email: string
+    startDateTime: string
+    endDateTime: string
+}
 function EventsPage() {
 
+    const [events, setEvents] = useState<Event[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/events')
+            .then(res => res.json())
+            .then(data => setEvents(data))
+    }, [])
+
   return (
-    <>
-     <h1>Main Page</h1>
-    </>
+    <div>
+        {events.map(event => (
+            <div key={event.id}>
+                <h2>{event.eventName}</h2>
+                <p>{event.location}</p>
+            </div>
+        ))}
+    </div>    
   )
 }
 
